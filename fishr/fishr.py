280ca39fda8ac5f52f5b846b1c92d0cfeec6b396
matthew-sochor <matthew.sochor@gmail.com>
2017-04-25 10:18:12 -0400

    Extract lat and lon from uploaded image meta data, if possible. Store results to DB

--- fishr/fishr.py
+++ fishr/fishr.py
@@ -13,6 +13,7 @@ from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
 
 from PIL import Image as pil_image
+from PIL.ExifTags import TAGS, GPSTAGS
 
 from .models import FishPic
 from .sqlite_queue import SqliteQueue
@@ -91,6 +92,12 @@ def upload():
             log.debug('Reading PIL: %s', fish_pic_file.filename)
             img = pil_image.open(fish_pic_file)
 
+            # Extract lat lon from Image if present
+            img_exif = get_exif_data(img)
+            img_lat_lon = get_lat_lon(img_exif)
+            log.debug('Lat: %s', img_lat_lon[0])
+            log.debug('Lon: %s', img_lat_lon[1])
+
             # get a random uuid to use for filename
             fish_pic_uuid = uuid.uuid4()
 
@@ -125,7 +132,9 @@ def upload():
             # TODO: make an ENV var
             fish_pic_db = FishPic('data/dbs/fishr.db')
             fish_pic_id = fish_pic_db.append({'img_path': img_path,
-                                              'img_path_sm': img_path_sm})
+                                              'img_path_sm': img_path_sm,
+                                              'latitude': img_lat_lon[0],
+                                              'longitude': img_lat_lon[1]})
 
             # push to scoring queue
             # TODO: make queue path a global in ENV
@@ -189,6 +198,69 @@ def get_fish_pic_dict(fish_pic_id):
 
         return fish_pic_dict
 
+def get_exif_data(image):
+    """Returns a dictionary from the exif data of an PIL Image item. Also converts the GPS Tags"""
+    exif_data = {}
+    info = image._getexif()
+    if info:
+        for tag, value in info.items():
+            decoded = TAGS.get(tag, tag)
+            if decoded == "GPSInfo":
+                gps_data = {}
+                for t in value:
+                    sub_decoded = GPSTAGS.get(t, t)
+                    gps_data[sub_decoded] = value[t]
+
+                exif_data[decoded] = gps_data
+            else:
+                exif_data[decoded] = value
+
+    return exif_data
+
+def _get_if_exist(data, key):
+    if key in data:
+        return data[key]
+    return None
+
+def _convert_to_degress(value):
+    """Helper function to convert the GPS coordinates stored in the EXIF to degress in float format"""
+    d0 = value[0][0]
+    d1 = value[0][1]
+    d = float(d0) / float(d1)
+
+    m0 = value[1][0]
+    m1 = value[1][1]
+    m = float(m0) / float(m1)
+
+    s0 = value[2][0]
+    s1 = value[2][1]
+    s = float(s0) / float(s1)
+
+    return d + (m / 60.0) + (s / 3600.0)
+
+def get_lat_lon(exif_data):
+    """Returns the latitude and longitude, if available, from the provided exif_data (obtained through get_exif_data above)"""
+    lat = None
+    lon = None
+
+    if "GPSInfo" in exif_data:
+        gps_info = exif_data["GPSInfo"]
+
+        gps_latitude = _get_if_exist(gps_info, "GPSLatitude")
+        gps_latitude_ref = _get_if_exist(gps_info, 'GPSLatitudeRef')
+        gps_longitude = _get_if_exist(gps_info, 'GPSLongitude')
+        gps_longitude_ref = _get_if_exist(gps_info, 'GPSLongitudeRef')
+        print(gps_latitude, gps_longitude)
+        if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
+            lat = _convert_to_degress(gps_latitude)
+            if gps_latitude_ref != "N":
+                lat = 0 - lat
+
+            lon = _convert_to_degress(gps_longitude)
+            if gps_longitude_ref != "E":
+                lon = 0 - lon
+
+    return lat, lon
 
 @app.route('/loading_splash/<int:fish_pic_id>')
 def loading_splash(fish_pic_id):
